2a106e653b82f64cc8a93e86287c06d3d8ab3794
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-03-29 20:13:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  
+  form.addEventListener('submit',(e)=>{
+  e.preventDefault()
+  handleToDo(e.target.querySelector('#new-task-description').value)
+  form.reset()
+  })
+  
 });
+function handleToDo (todo){
+  let li = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener('click', HandleDelete)
+  btn.textContent = "Delete"
+  li.textContent = todo
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li)
+  console.log(li);
+}
+
+function HandleDelete(e){
+  e.target.parentElement.remove()
+}
+
